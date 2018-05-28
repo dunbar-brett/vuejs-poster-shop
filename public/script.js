66c5ed6d87379358c6945a550879ac3204ac9153
dunbar-brett <dunbar.brett@gmail.com>
2018-05-27 17:36:36 -0700

    added items and cart with add to cart button.

--- public/script.js
+++ public/script.js
@@ -1,1 +1,21 @@
-console.log('It works.');
+new Vue({
+    el: '#app',
+    data: {
+        total: 0,
+        items: [
+            {title: 'Item 1'},
+            {title: 'Item 2'},
+            {title: 'Item 3'}
+        ],
+        cart: []
+    },
+    methods: {
+        addItem: function(index) {
+            // TODO change this to update on movie.price
+            this.total += 9.99;
+            this.cart.push(this.items[index]);
+        }
+    }
+
+    //template: ''
+});
