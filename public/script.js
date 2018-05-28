90e6086823a686639d50dfad50cc2b578ead4009
dunbar-brett <dunbar.brett@gmail.com>
2018-05-27 20:37:01 -0700

    methods: addItem, inc, dec, currency filter. frontend: inc and dec buttons, beginning of serach bar

--- public/script.js
+++ public/script.js
@@ -3,19 +3,56 @@ new Vue({
     data: {
         total: 0,
         items: [
-            {title: 'Item 1'},
-            {title: 'Item 2'},
-            {title: 'Item 3'}
+            { id: 1, title: 'Item 1', price: 9.99 },
+            { id: 2, title: 'Item 2', price: 19.99 },
+            { id: 3, title: 'Item 3', price: 29.91 }
         ],
-        cart: []
+        cart: [],
+        search: ''
+    },
+    filters: {
+        toCurrency: function(price) {
+            return '$'.concat(price.toFixed(2));
+        }
     },
     methods: {
-        addItem: function(index) {
-            // TODO change this to update on movie.price
-            this.total += 9.99;
-            this.cart.push(this.items[index]);
+        onSubmit: function () {
+            // TODO filter to what the search passes using vue-resource or some shit
+        },
+        addItem: function (index) {
+            var item = this.items[index];
+            var isItemInCart = this.cart.some(f => f.id == item.id);
+
+            if(!isItemInCart){
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    price: item.price,
+                    qty: 1
+                });
+                this.total += item.price;
+            } else {
+                inc(item);
+            }
+        },
+        inc: function(item) {
+            this.cart.filter(function(e) {
+                if (e.id == item.id) {
+                    e.qty += 1;
+                }                    
+            });
+            this.total += item.price;
+        },
+        dec: function(item, index) {
+            this.cart.filter(function(e) {
+                if (e.id == item.id) {
+                    e.qty -= 1;
+                }                     
+            });
+            if(item.qty == 0) {
+                this.cart.splice(index, 1);
+            }
+            this.total -= item.price;
         }
     }
-
-    //template: ''
 });
